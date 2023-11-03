45ea172663b26f32d302455ccc848cf764dd643c
Rishav Jain <rishav@Rishav-ka-MacBook-Air.local>
2023-11-03 10:12:27 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:app1/gradient_container.dart';
 
+//changig
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer([
-          Colors.purple,
-          Colors.black
-        ]),
+        body: GradientContainer([Colors.purple, Colors.black]),
       ),
     ),
   );
 }
-
